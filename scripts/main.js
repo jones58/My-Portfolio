e463b9345ed201de136490e626f21f986ae3fecd
jones58 <108466184+jones58@users.noreply.github.com>
2023-10-09 11:31:03 +0100

    removing plain text mode to make site more simple

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,10 @@ const darkModeButton = document.getElementById("dark-mode");
 
 const darkModeOn = () => {
   document.body.classList.add("darkMode");
-  document.body.classList.remove("plainTextMode");
 };
 
 const darkModeOff = () => {
   document.body.classList.remove("darkMode");
-  document.body.classList.remove("plainTextMode");
 };
 
 darkModeButton.addEventListener("click", () => {
@@ -19,63 +17,3 @@ darkModeButton.addEventListener("click", () => {
     darkModeOn();
   }
 });
-
-//plain text //
-const plaintextButton = document.getElementById("plain-text");
-
-const plainTextOn = () => {
-  document.body.classList.add("plainTextMode");
-  document.body.classList.remove("darkMode");
-};
-
-const plainTextOff = () => {
-  document.body.classList.remove("plainTextMode");
-};
-
-plaintextButton.addEventListener("click", () => {
-  if (document.body.classList.contains("plainTextMode")) {
-    plainTextOff();
-  } else {
-    plainTextOn();
-  }
-});
-
-//image change //
-
-const imgButton = document.getElementById("header-img");
-
-imgButton.addEventListener("click", () => {
-  if (imgButton.getAttribute("src") === "images/me-image.jpg") {
-    imgButton.setAttribute("src", "images/me-image-new.jpg");
-    imgButton.setAttribute("title", "My head fits through this one");
-    imgButton.setAttribute(
-      "alt",
-      "a photo of me, Jack, aged six, poking my head through a hole in a wall"
-    );
-  } else {
-    imgButton.setAttribute("src", "images/me-image.jpg");
-    imgButton.setAttribute("title", "My head doesn't fit anymore");
-    imgButton.setAttribute(
-      "alt",
-      "a photo of me, Jack, aged twenty-five, poking my head through a hole in a wall"
-    );
-  }
-});
-
-// keydown to make more text appear //
-
-const textToShow = document.getElementById("extra-text1");
-let count = 0;
-document.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowDown") {
-    count++;
-    if (count === 1) {
-      document.body.classList.add("showmeOne");
-    }
-    if (count === 2) {
-      document.body.classList.add("showmeTwo");
-    }
-  }
-});
-
-const textToShow2 = document.getElementById("extra-text2");
