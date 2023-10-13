23d45659ca7dd69ba3d7860f3ecb56e228ca87ee
jones58 <108466184+jones58@users.noreply.github.com>
2023-10-13 10:51:59 +0100

    removing php js config
    switching html to php to repeat footer and header
    
    editing php docs to work together
    
    removing js php config

---
+++ design.php
@@ -0,0 +1,12 @@
+<?php
+        include('header.php');
+    ?>
+<div class="page" id="projects">
+      <p1 class="medium-introduction">
+        Section in progress! Come back soon...</p1
+      >
+    </div>
+  </body>
+  <?php
+        include('footer.php');
+    ?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+        include('includes/header.php');
+    ?>
+<div class="page" id="about-me">
+  <h4 id="big-introduction">
+    Hi there! I'm Jack, a London-based designer and developer. I'm
+    currently studying software development with Just IT.
+  </h4>
+</div>
+<?php
+        include('footer.php');
+    ?>
